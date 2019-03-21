0c54c83e2d9b50167c727a9caafde22eb6320d15
D7703_18 <d7703_18@d7703-18ui-imac.local>
2019-03-21 16:50:44 +0900

    완료

--- buttongo/ViewController.swift
+++ buttongo/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mylabel: UILabel!
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var countlabel: UILabel!
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        countlabel.text = String(count)
     }
-
+    @IBAction func buttonressed(_ sender: Any) {
+        count = count + 1
+        mylabel.text = "잘 되네"
+        countlabel.text = String(count)
+    }
+    @IBAction func button(_ sender: Any) {
+       // mylabel.text = "잘 되냐?"
+        label.text = "잘 되고있냐?"
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
